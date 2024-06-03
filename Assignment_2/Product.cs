630ba0053a3c0457e1e1c2f7bb7a8d23cfbcfa20
HarishM99 <160193294+HarishM99@users.noreply.github.com>
2024-06-03 15:13:55 -0400

    Tests Run Successful

--- Assignment_2/Product.cs
+++ Assignment_2/Product.cs
@@ -11,23 +11,23 @@ namespace Assignment_2
     {
         public int ProductID { get; set; }
         public string ProductName { get; set; }
-        public decimal ProductPrice { get; set; }
+        public decimal Price { get; set; }
         public int Stock { get; set; }
 
-        public Product(int productID, string productName, decimal productPrice, int stock)
+        public Product(int productID, string productName, decimal price, int stock)
         {
             if (productID < 1 || productID > 5000)
                 throw new ArgumentException("ProductID must be in between 1 to 5000 ");
             if (string.IsNullOrEmpty(productName))
                 throw new ArgumentException("ProductName cannot be null or empty.");
-            if (productPrice < 1 || productPrice > 10000)
+            if (price < 1 || price > 10000)
                 throw new ArgumentException("Price must be between $1 and $5000.");
             if (stock < 1 || stock > 5000)
                 throw new ArgumentException("Stock must be between 1 and 1000.");
 
             ProductID = productID;
             ProductName = productName;
-            ProductPrice = productPrice;
+            Price = price;
             Stock = stock;
         }
 
